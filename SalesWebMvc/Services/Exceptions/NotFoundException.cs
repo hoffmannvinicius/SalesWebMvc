bf1ca4976ad4029d78d9a1b8f76beb8ea566a0b2
Vinicius <vinicius_lemao.oliver@hotmail.com>
2021-12-05 16:19:21 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+
+        public NotFoundException(string message) : base(message) // Excessão personalizada
+        {
+
+        }
+
+    }
+}
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 
 namespace SalesWebMvc.Services
@@ -12,12 +13,12 @@ namespace SalesWebMvc.Services
     public class SellerService
     {
 
-        private readonly  SalesWebMvcContext _context; // Criando variavel do tipo context
+        private readonly SalesWebMvcContext _context; // Criando variavel do tipo context
 
 
         public SellerService(SalesWebMvcContext context)
         {
-            _context = context;
+            _context=context;
         }
 
         public List<Seller> FindAll()
@@ -27,7 +28,7 @@ namespace SalesWebMvc.Services
 
 
         public void Insert(Seller obj)
-        
+
         {
             _context.Add(obj);
             _context.SaveChanges();
@@ -35,7 +36,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id==id);
         }
 
         public void Remove(int id)
@@ -43,6 +44,28 @@ namespace SalesWebMvc.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+
+        public void Update(Seller obj)
+        {
+
+            if(!_context.Seller.Any(x => x.Id==obj.Id))
+            {
+                throw new NotFoundException("Id não encontrado");
+            }
+
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
+
         }
 
 
